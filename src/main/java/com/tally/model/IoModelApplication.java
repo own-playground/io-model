53a0c21da793958d332780cc6ced45d74e02afad
ahn-sj <jaesa5221@gmail.com>
2024-07-06 21:32:58 +0900

    Initial Project

---
+++ src/main/java/com/tally/model/IoModelApplication.java
@@ -0,0 +1,13 @@
+package com.tally.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tally/model/IoModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
