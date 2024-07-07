99c593723db983eccb5d32a7d4c716aaaac9d40a
ahn-sj <jaesa5221@gmail.com>
2024-07-07 22:50:56 +0900

    refactor: Asynchronous & Non-Blocking

--- src/main/java/com/tally/model/asynchronous/AsynchronousService.java
+++ src/main/java/com/tally/model/asynchronous/AsynchronousService.java
@@ -4,7 +4,17 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Asynchronous & Non-blocking
+ * description: 비동기적으로 작업을 시작하고, 논블로킹 방식으로 결과를 기다린다.
+ * 논블로킹 방식이기 때문에 메인 스레드는 다른 작업을 수행할 수 있고, 작업이 완료되었을 때 콜백을 통해 결과를 반환한다.
+ */
 public class AsynchronousService {
+
+    public static void main(String[] args) {
+        AsynchronousService instance = new AsynchronousService();
+        instance.asyncNonBlocking();
+    }
 
     private static final Map<Long, String> repository = new ConcurrentHashMap<>();
 
@@ -14,72 +24,44 @@ public class AsynchronousService {
         repository.put(3L, "이영희");
     }
 
-    public static void main(String[] args) {
-        AsynchronousService instance = new AsynchronousService();
-        instance.execute();
-    }
+    public void asyncNonBlocking() {
+        System.out.println("===== [Async][Non-Blocking] - 시작 =====");
 
-    public void execute() {
-        System.out.println("Starting ");
-
-        CompletableFuture<String> futureA = getById(2L, 2000); // 2초 후 완료
-        CompletableFuture<String> futureB = getById(3L, 1000); // 1초 후 완료
-
-        futureA.thenAccept(name -> {
-            System.out.println("A 함수 완료: name = " + name);
+        CompletableFuture<String> futureA = CompletableFuture.supplyAsync(() -> {
+            for (int i = 1; i <= 5; i++) {
+                sleep(200);
+                System.out.println("[비동기 작업] 작업A 진행중... " + i);
+            }
+            return repository.get(2L);  // 2초 후 완료
+        });
+        CompletableFuture<String> futureB = CompletableFuture.supplyAsync(() -> {
+            for (int i = 1; i <= 5; i++) {
+                sleep(100);
+                System.out.println("[비동기 작업] 작업B 진행중... " + i);
+            }
+            return repository.get(3L);  // 1초 후 완료
         });
 
-        futureB.thenAccept(result -> {
-            System.out.println("B 함수 완료: name = " + result);
-            System.out.println("=======  complete execute =======");
-        });
+        CompletableFuture.allOf(futureA, futureB)
+                .thenRun(() -> System.out.println("------ 모든 비동기 작업 완료 ------"))
+                .thenCompose(v -> futureA).thenAccept(result -> System.out.println("complete futureA result = " + result))
+                .thenCompose(v -> futureB).thenAccept(result ->System.out.println("complete futureB result = " + result));
 
-        System.out.println("비동기 작업 시작");
-
-        while (!futureA.isDone() || !futureB.isDone()) {
-            if (!futureA.isDone()) {
-                System.out.println("A 작업 진행 중...");
-            }
-            if (!futureB.isDone()) {
-                System.out.println("B 작업 진행 중...");
-            }
-            try {
-                Thread.sleep(100); // 짧은 시간 대기 후 다시 확인
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        System.out.println("메인 스레드는 비동기 작업의 완료를 기다리지 않고 계속 실행됩니다.");
+        for (int i = 1; i <= 5; i++) {
+            sleep(300);
+            System.out.println(">> [메인 스레드] 작업  진행중... " + i);
         }
-        System.out.println("모든 비동기 작업 완료");
+        System.out.println("메인 스레드 작업 완료");
+
+        System.out.println("===== [Async][Non-Blocking] - 종료 =====");
     }
 
-    /** 비동기 작업 A, B를 수행하는데 A가 먼저 끝나고 B가 다음에 끝남 */
-    /**
-     * public void execute() {
-     *     System.out.println("Starting Case A");
-     *
-     *     CompletableFuture<String> futureA = getById(2L, 1000); // 1초 후 완료
-     *     futureA.thenAccept(name -> {
-     *         System.out.println("Case A - A 함수 완료: name = " + name);
-     *         // A 함수 완료 후 B 함수 호출
-     *         CompletableFuture<String> futureB = getById(3L, 2000); // 2초 후 완료
-     *         futureB.thenAccept(result -> {
-     *             System.out.println("Case A - B 함수 완료: name = " + result);
-     *             System.out.println("======= Case A complete execute =======");
-     *         });
-     *     });
-     *
-     *     System.out.println("Case A - 비동기 작업 시작");
-     * }
-     */
-
-    private CompletableFuture<String> getById(final Long id, int delay) {
-        return CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(delay); // I/O 작업 시뮬레이션
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return repository.get(id);
-        });
+    private static void sleep(final int millis) {
+        try {
+            Thread.sleep(millis); // 가상의 I/O 작업 시간
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
